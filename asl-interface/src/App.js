b515674c5facf5b2e4d5c19d58aa6d7e7ebbe26f
Ngoc Huynh <97254009+ngocvbh@users.noreply.github.com>
2024-01-26 20:29:26 -0800

    base interface added

---
+++ asl-interface/src/App.js
@@ -0,0 +1,13 @@
+import Webcam from "react-webcam";
+import { useRef } from "react";
+
+function App() {
+  const webRef = useRef(null);
+  return (
+    <div className="App">
+      <Webcam ref={webRef}/>
+    </div>
+  );
+}
+
+export default App;
